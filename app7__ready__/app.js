f85c9eac79f19c5a145a67f0e84f0688b6510e96
InSeong-So <goflvhxj2547@gmail.com>
2021-07-17 22:51:11 +0900

    chore: Build app7__ready__ : sortable drag&drop

---
+++ app7__ready__/app.js
@@ -0,0 +1,45 @@
+const draggables = document.querySelectorAll('.draggable');
+const containers = document.querySelectorAll('.container');
+
+draggables.forEach(draggable => {
+  draggable.addEventListener('dragstart', () => {
+    draggable.classList.add('dragging');
+  });
+
+  draggable.addEventListener('dragend', () => {
+    draggable.classList.remove('dragging');
+  });
+});
+
+containers.forEach(container => {
+  container.addEventListener('dragover', event => {
+    event.preventDefault();
+    const afterElement = getDragAfterElement(container, event.clientY);
+    const draggable = document.querySelector('.dragging');
+    if (afterElement === null) {
+      container.appendChild(draggable);
+    } else {
+      container.insertBefore(draggable, afterElement);
+    }
+  });
+});
+
+function getDragAfterElement(container, y) {
+  const draggableElements = [
+    ...container.querySelectorAll('.draggable:not(.dragging)'),
+  ];
+  return draggableElements.reduce(
+    (closest, child) => {
+      const box = child.getBoundingClientRect();
+      const offset = y - box.top - box.height / 2;
+      if (offset < 0 && offset > closest.offset) {
+        return { offset: offset, element: child };
+      } else {
+        return closest;
+      }
+    },
+    {
+      offset: Number.NEGATIVE_INFINITY,
+    },
+  ).element;
+}
